87bce3eb292d1dbcb38e1029950534f398650a94
Masaki Takeuchi <m.ishihara@gmail.com>
2017-01-06 05:39:11 +0000

    bundle gem cloudwatch-metrics-linux --exe --coc --mit

---
+++ lib/cloudwatch/metrics/linux.rb
@@ -0,0 +1,9 @@
+require "cloudwatch/metrics/linux/version"
+
+module Cloudwatch
+  module Metrics
+    module Linux
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/cloudwatch/metrics/linux/version.rb
@@ -0,0 +1,7 @@
+module Cloudwatch
+  module Metrics
+    module Linux
+      VERSION = "0.1.0"
+    end
+  end
+end
